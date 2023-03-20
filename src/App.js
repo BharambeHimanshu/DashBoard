1a2bb411c0a05050adbe0ab467608126a571224d
Himanshu Bharambe <himanshubharambe04@gmail.com>
2023-03-20 22:36:41 +0530

    remove unwanted files

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import Topbar from "./scenes/global/Topbar";
 import Sidebar from "./scenes/global/Sidebar";
 import Dashboard from "./scenes/dashboard";
-import Team from "./scenes/team";
-import Invoices from "./scenes/invoices";
 import PendingOrder from "./scenes/PendingOrder";
 import EnrouteForPickup from "./scenes/EnrouteForPickup";
 import AtPickup from "./scenes/AtPickup";
 import Intransit from "./scenes/Intransit";
 import AtUnloading from "./scenes/AtUnloading";
 import Complete from "./scenes/Complete";
-import Bar from "./scenes/bar";
-import Form from "./scenes/form";
-import Line from "./scenes/line";
-import Pie from "./scenes/pie";
-import FAQ from "./scenes/faq";
-import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import Calendar from "./scenes/calendar/calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -35,21 +26,12 @@ function App() {
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
               <Route path="/" element={<Dashboard />} />
-              <Route path="/team" element={<Team />} />
               <Route path="/pendingorder" element={<PendingOrder />} />
               <Route path="/enrouteforpickup" element={<EnrouteForPickup />} />
               <Route path="/atpickup" element={<AtPickup />} />
               <Route path="/intransit" element={<Intransit />} />
               <Route path="/atunloading" element={<AtUnloading />} />
               <Route path="/completed" element={<Complete />} />
-              <Route path="/invoices" element={<Invoices />} />
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/pie" element={<Pie />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
-              <Route path="/geography" element={<Geography />} />
             </Routes>
           </main>
         </div>
